c386cbfc18d045b3505d0bc590502ca0f39622d0
Luke Azzopardi <lukeazzopardi@Lukes-Mac-mini.local>
2026-02-03 18:29:13 +0100

    Initial launch

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@
   <meta name="CocoaVersion" content="2685.3">
   <style type="text/css">
     p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; -webkit-text-stroke: #000000}
-    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; color: #113210; -webkit-text-stroke: #113210}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; color: #175f20; -webkit-text-stroke: #175f20}
     p.p3 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; -webkit-text-stroke: #000000; min-height: 16.0px}
-    p.p4 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; color: #1a1b1d; -webkit-text-stroke: #1a1b1d}
-    span.s1 {font-kerning: none; color: #270084; background-color: #d2dbeb; -webkit-text-stroke: 0px #270084}
-    span.s2 {font-kerning: none; background-color: #d2dbeb}
-    span.s3 {font-kerning: none; color: #113210; background-color: #d2dbeb; -webkit-text-stroke: 0px #113210}
-    span.s4 {font-kerning: none; background-color: #d2dbeb; -webkit-text-stroke: 0px #000000}
+    p.p4 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; color: #3c3f43; -webkit-text-stroke: #3c3f43}
+    span.s1 {font-kerning: none; color: #5a00b6; background-color: #e7edf5; -webkit-text-stroke: 0px #5a00b6}
+    span.s2 {font-kerning: none; background-color: #e7edf5}
+    span.s3 {font-kerning: none; color: #175f20; background-color: #e7edf5; -webkit-text-stroke: 0px #175f20}
+    span.s4 {font-kerning: none; background-color: #e7edf5; -webkit-text-stroke: 0px #000000}
     span.s5 {font-kerning: none}
   </style>
 </head>
@@ -26,7 +26,7 @@
 <p class="p1"><span class="s1">export</span><span class="s2"> </span><span class="s1">default</span><span class="s2"> defineConfig({</span></p>
 <p class="p1"><span class="s2"><span class="Apple-converted-space">  </span>plugins: [react()],</span></p>
 <p class="p4"><span class="s4"><span class="Apple-converted-space">  </span></span><span class="s2">// IMPORTANT: The base path must match your repository name for GitHub Pages to work.</span></p>
-<p class="p4"><span class="s4"><span class="Apple-converted-space">  </span></span><span class="s2">// If your repo is https://github.com/username/my-tool, base should be '/my-tool/'</span></p>
+<p class="p4"><span class="s4"><span class="Apple-converted-space">  </span></span><span class="s2">// If your repo is https://github.com/username/switch-timesheet, base should be '/switch-timesheet/'</span></p>
 <p class="p2"><span class="s4"><span class="Apple-converted-space">  </span>base: </span><span class="s2">‘/switch-timesheet/‘</span><span class="s4">,<span class="Apple-converted-space"> </span></span></p>
 <p class="p1"><span class="s2">})</span></p>
 </body>
