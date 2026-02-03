d146af2d200005d5d8f1070a505a2b0410b539d6
Luke Azzopardi <lukeazzopardi@Lukes-Mac-mini.local>
2026-02-03 18:05:47 +0100

    Initial Launch

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2685.3">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; -webkit-text-stroke: #000000}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; color: #113210; -webkit-text-stroke: #113210}
+    p.p3 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; -webkit-text-stroke: #000000; min-height: 16.0px}
+    p.p4 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Menlo; color: #1a1b1d; -webkit-text-stroke: #1a1b1d}
+    span.s1 {font-kerning: none; color: #270084; background-color: #d2dbeb; -webkit-text-stroke: 0px #270084}
+    span.s2 {font-kerning: none; background-color: #d2dbeb}
+    span.s3 {font-kerning: none; color: #113210; background-color: #d2dbeb; -webkit-text-stroke: 0px #113210}
+    span.s4 {font-kerning: none; background-color: #d2dbeb; -webkit-text-stroke: 0px #000000}
+    span.s5 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">import</span><span class="s2"> { defineConfig } </span><span class="s1">from</span><span class="s2"> </span><span class="s3">'vite'</span></p>
+<p class="p2"><span class="s1">import</span><span class="s4"> react </span><span class="s1">from</span><span class="s4"> </span><span class="s2">'@vitejs/plugin-react'</span></p>
+<p class="p3"><span class="s5"></span><br></p>
+<p class="p4"><span class="s2">// https://vitejs.dev/config/</span></p>
+<p class="p1"><span class="s1">export</span><span class="s2"> </span><span class="s1">default</span><span class="s2"> defineConfig({</span></p>
+<p class="p1"><span class="s2"><span class="Apple-converted-space">  </span>plugins: [react()],</span></p>
+<p class="p4"><span class="s4"><span class="Apple-converted-space">  </span></span><span class="s2">// IMPORTANT: The base path must match your repository name for GitHub Pages to work.</span></p>
+<p class="p4"><span class="s4"><span class="Apple-converted-space">  </span></span><span class="s2">// If your repo is https://github.com/username/my-tool, base should be '/my-tool/'</span></p>
+<p class="p2"><span class="s4"><span class="Apple-converted-space">  </span>base: </span><span class="s2">‘/switch-timesheet/‘</span><span class="s4">,<span class="Apple-converted-space"> </span></span></p>
+<p class="p1"><span class="s2">})</span></p>
+</body>
+</html>
